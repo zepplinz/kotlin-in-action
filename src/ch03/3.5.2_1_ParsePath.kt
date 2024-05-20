b9c8f6d88534f933cbe05861b37c048e0f07103e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-20 20:24:28 +0000

    edit_file

--- src/ch03/3.5.2_1_ParsePath.kt
+++ src/ch03/3.5.2_1_ParsePath.kt
@@ -1,15 +1,8 @@
-package ch03.ParsePath
-
-fun parsePath(path: String) {
-    val directory = path.substringBeforeLast("/")
-    val fullName = path.substringAfterLast("/")
-
-    val fileName = fullName.substringBeforeLast(".")
-    val extension = fullName.substringAfterLast(".")
-
-    println("Dir: $directory, name: $fileName, ext: $extension")
-}
-
-fun main(args: Array<String>) {
-    parsePath("/Users/yole/kotlin-book/chapter.adoc")
-}
+function parsePath(path: string): void {
+    const directory = path.substring(0, path.lastIndexOf("/"));
+    const fullName = path.substring(path.lastIndexOf("/") + 1);
+    const fileName = fullName.substring(0, fullName.lastIndexOf("."));
+    const extension = fullName.substring(fullName.lastIndexOf(".") + 1);
+    console.log(`Dir: ${directory}, name: ${fileName}, ext: ${extension}`);
+function main(args: string[]): void {
+    parsePath("/Users/yole/kotlin-book/chapter.adoc");
