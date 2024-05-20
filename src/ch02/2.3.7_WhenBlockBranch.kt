a03c8ec6ccd2ddf23eab025d4926c34dde4aa615
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-20 20:21:40 +0000

    edit_file

--- src/ch02/2.3.7_WhenBlockBranch.kt
+++ src/ch02/2.3.7_WhenBlockBranch.kt
@@ -1,24 +1,32 @@
-package ch02.ex3_7_WhenBlockBranch
-
-interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
-
-fun evalWithLogging(e: Expr): Int =
-    when (e) {
-        is Num -> {
-            println("num: ${e.value}")
-            e.value
-        }
-        is Sum -> {
-            val left = evalWithLogging(e.left)
-            val right = evalWithLogging(e.right)
-            println("sum: $left + $right")
-            left + right
-        }
-        else -> throw IllegalArgumentException("Unknown expression")
+interface Expr {}
+class Num implements Expr {
+    value: number;
+    constructor(value: number) {
+        this.value = value;
     }
-
-fun main(args: Array<String>) {
-    println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
 }
+class Sum implements Expr {
+    left: Expr;
+    right: Expr;
+    constructor(left: Expr, right: Expr) {
+        this.left = left;
+        this.right = right;
+    }
+}
+function evalWithLogging(e: Expr): number {
+    if (e instanceof Num) {
+        console.log(`num: ${e.value}`);
+        return e.value;
+    } else if (e instanceof Sum) {
+        const left = evalWithLogging(e.left);
+        const right = evalWithLogging(e.right);
+        console.log(`sum: ${left} + ${right}`);
+        return left + right;
+    } else {
+        throw new Error("Unknown expression");
+    }
+}
+function main() {
+    console.log(evalWithLogging(new Sum(new Sum(new Num(1), new Num(2)), new Num(4))));
+}
+main();
