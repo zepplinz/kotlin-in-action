4be1a0140758c54dda83379542d246d0c2d5b282
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-20 20:21:18 +0000

    edit_file

--- src/ch02/2.3.3_UsingWhenWithArbitraryObjects.kt
+++ src/ch02/2.3.3_UsingWhenWithArbitraryObjects.kt
@@ -1,16 +1,25 @@
-package ch02.ex3_3_UsingWhenWithArbitraryObjects
-
-import ch02.colors.Color
-import ch02.colors.Color.*
-
-fun mix(c1: Color, c2: Color) =
-        when (setOf(c1, c2)) {
-            setOf(RED, YELLOW) -> ORANGE
-            setOf(YELLOW, BLUE) -> GREEN
-            setOf(BLUE, VIOLET) -> INDIGO
-            else -> throw Exception("Dirty color")
-        }
-
-fun main(args: Array<String>) {
-    println(mix(BLUE, YELLOW))
+enum Color {
+    RED,
+    YELLOW,
+    BLUE,
+    VIOLET,
+    ORANGE,
+    GREEN,
+    INDIGO
 }
+function mix(c1: Color, c2: Color): Color {
+    const colorSet = new Set([c1, c2]);
+    if (colorSet.has(Color.RED) && colorSet.has(Color.YELLOW)) {
+        return Color.ORANGE;
+    } else if (colorSet.has(Color.YELLOW) && colorSet.has(Color.BLUE)) {
+        return Color.GREEN;
+    } else if (colorSet.has(Color.BLUE) && colorSet.has(Color.VIOLET)) {
+        return Color.INDIGO;
+    } else {
+        throw new Error("Dirty color");
+    }
+}
+function main() {
+    console.log(mix(Color.BLUE, Color.YELLOW));
+}
+main();
