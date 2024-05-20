e2ad5c9eb7c46361fd2f3164ba1b4141c423b830
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-20 20:22:19 +0000

    edit_file

--- src/ch02/2.3.1_DeclaringEnumClasses.kt
+++ src/ch02/2.3.1_DeclaringEnumClasses.kt
@@ -1,15 +1,31 @@
-package ch02.ex3_1_DeclaringEnumClasses
-
-enum class Color(
-        val r: Int, val g: Int, val b: Int
-) {
-    RED(255, 0, 0), ORANGE(255, 165, 0),
-    YELLOW(255, 255, 0), GREEN(0, 255, 0), BLUE(0, 0, 255),
-    INDIGO(75, 0, 130), VIOLET(238, 130, 238);
-
-    fun rgb() = (r * 256 + g) * 256 + b
+enum Color {
+    RED = "RED",
+    ORANGE = "ORANGE",
+    YELLOW = "YELLOW",
+    GREEN = "GREEN",
+    BLUE = "BLUE",
+    INDIGO = "INDIGO",
+    VIOLET = "VIOLET"
 }
-
-fun main(args: Array<String>) {
-    println(Color.BLUE.rgb())
+interface RGB {
+    r: number;
+    g: number;
+    b: number;
 }
+const colorValues: { [key in Color]: RGB } = {
+    [Color.RED]: { r: 255, g: 0, b: 0 },
+    [Color.ORANGE]: { r: 255, g: 165, b: 0 },
+    [Color.YELLOW]: { r: 255, g: 255, b: 0 },
+    [Color.GREEN]: { r: 0, g: 255, b: 0 },
+    [Color.BLUE]: { r: 0, g: 0, b: 255 },
+    [Color.INDIGO]: { r: 75, g: 0, b: 130 },
+    [Color.VIOLET]: { r: 238, g: 130, b: 238 }
+};
+function rgb(color: Color): number {
+    const { r, g, b } = colorValues[color];
+    return (r * 256 + g) * 256 + b;
+}
+function main() {
+    console.log(rgb(Color.BLUE));
+}
+main();
