c5156dc3f48ae4b5306e9ec9180cf2fc817d7ab8
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-22 20:48:12 +0000

    edit_file

--- src/ch04/4.3.2.1_DataClassCopy.kt
+++ src/ch04/4.3.2.1_DataClassCopy.kt
@@ -1,8 +1,15 @@
-package ch04.ex3_2_1_DataClassCopy
+type Client = {
+    name: string;
+    postalCode: number;
+};
 
-data class Client(val name: String, val postalCode: Int)
-
-fun main(args: Array<String>) {
-    val bob = Client("Bob", 973293)
-    println(bob.copy(postalCode = 382555))
+function copyClient(client: Client, newPostalCode: number): Client {
+    return { ...client, postalCode: newPostalCode };
 }
+
+function main() {
+    const bob: Client = { name: "Bob", postalCode: 973293 };
+    console.log(copyClient(bob, 382555));
+}
+
+main();
