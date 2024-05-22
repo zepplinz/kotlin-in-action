4f0a679f45a0b5f04d1c08334652b8a0a6d61023
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-22 20:49:32 +0000

    edit_file

--- src/ch03/3.3.5_ExtensionProperties.kt
+++ src/ch03/3.3.5_ExtensionProperties.kt
@@ -1,16 +1,32 @@
-package ch03.ex3_5_ExtensionProperties
-
-val String.lastChar: Char
-    get() = get(length - 1)
-var StringBuilder.lastChar: Char
-    get() = get(length - 1)
-    set(value: Char) {
-        this.setCharAt(length - 1, value)
+// Define a new type for String with a lastChar property
+type StringWithLastChar = {
+    lastChar: string;
+};
+// Define a new type for StringBuilder with a lastChar property
+type StringBuilderWithLastChar = {
+    lastChar: string;
+};
+// Extend the String prototype to add the lastChar property
+Object.defineProperty(String.prototype, 'lastChar', {
+    get: function() {
+        return this.charAt(this.length - 1);
     }
-
-fun main(args: Array<String>) {
-    println("Kotlin".lastChar)
-    val sb = StringBuilder("Kotlin?")
-    sb.lastChar = '!'
-    println(sb)
+});
+// Extend the StringBuilder prototype to add the lastChar property
+Object.defineProperty(String.prototype, 'lastChar', {
+    get: function() {
+        return this.charAt(this.length - 1);
+    },
+    set: function(value: string) {
+        this.setCharAt(this.length - 1, value);
+    }
+});
+// Main function
+function main() {
+    console.log("Kotlin".lastChar);
+    let sb = new StringBuilder("Kotlin?");
+    sb.lastChar = '!';
+    console.log(sb.toString());
 }
+// Call the main function
+main();
