8f64f3829a7b295e8688245fb83683652d758b3d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-22 20:47:23 +0000

    edit_file

--- src/ch04/4.4.2_1_CompanionObjects.kt
+++ src/ch04/4.4.2_1_CompanionObjects.kt
@@ -1,13 +1,9 @@
-package ch04.ex4_2_1_CompanionObjects
-
 class A {
-    companion object {
-        fun bar() {
-            println("Companion object called")
-        }
+    static bar() {
+        console.log("Companion object called");
     }
 }
-
-fun main(args: Array<String>) {
-    A.bar()
+function main(args: string[]) {
+    A.bar();
 }
+main([]);
